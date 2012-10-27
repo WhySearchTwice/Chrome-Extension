60bf0371e2b0ebb8bda01cf9ab89a92f21f4683d
Tony Grosinger <tony@grosinger.net>
2012-10-27 23:02:01 +0000

    More testing

--- storageEngine/server.py
+++ storageEngine/server.py
@@ -16,17 +16,9 @@ def home():
 
 @app.route('/capstone/pageview', methods=['POST'])
 def savePageview():
-    print("starting pageview push")
-
     if request.method == 'POST':
-        print("Request is post")
         # Retrieve the data from the request
-        postData = request.json
-        print("retrieved post data")
-
-        # Validate the request contained JSON
-        if postData is None:
-            return 'Invalid request type. Expecting mimetype application/json'
+        print request.data
 
     return 'A work in progress'
 
