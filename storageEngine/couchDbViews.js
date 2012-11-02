8544b91483303acc6dd3fd4c1e27d10c24d915e4
Tony Grosinger <tony@grosinger.net>
2012-11-02 13:13:23 -0700

    Created storage for views that will be put into couchDB

---
+++ storageEngine/couchDbViews.js
@@ -0,0 +1,26 @@
+{
+   "_id": "_design/pageView",
+   "_rev": "1-5207d9b532a69c99c84892efd2c11b0b",
+   "language": "javascript",
+   "views": {
+       "all": {
+           "map": "function(doc) { if (doc.type == 'pageView')  emit(null, doc) }"
+       },
+       "by_userId": {
+           "map": "function(doc) { if (doc.type == 'pageView')  emit(doc.userId, doc) }"
+       }
+   }
+}
+
+{
+   "_id": "_design/stats",
+   "language": "javascript",
+   "views": {
+       "hitCountByDomain": {
+           "map": "function(doc) { if (doc.type == 'pageView')  emit(null, doc) }"
+       },
+       "by_userId": {
+           "map": "function(doc) { if (doc.type == 'pageView')  emit(doc.userId, doc) }"
+       }
+   }
+}
