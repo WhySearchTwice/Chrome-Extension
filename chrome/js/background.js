4050f215a6e9c56178192818da4380a02d7d220b
Tony Grosinger <tony@grosinger.net>
2012-11-11 12:09:07 -0800

    Coded out a large part of the comments that I made earlier

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,17 +1,16 @@
 // Library of page views yet to be submitted
 // When a page is navigated away from, the view will be submitted and removed
-var pageViewStorage = {
-    history: []
-};
+var pageViewStorage = {};
 
 // Listen for when a window is closed (submit all pages that are open in this window)
 chrome.windows.onRemoved.addListener(function(windowId) {
-    for (var tab in pageViewStorage.windows[windowId]) {
-        // Add a time closed
-
+    for (var tabId in pageViewStorage.windows[windowId]) {
         // Send this tab and remove
-        sendAndDeleteTab(windowId, tab.tabId);
+        sendAndDeleteTab(windowId, tab);
     }
+
+    // Remove the window from the storage
+    delete pageViewStorage.windows[windowId];
 });
 
 // Listen for a page being loaded or updated
@@ -26,20 +25,46 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         };
 
         // Check if there is a tab with this ID already. If so, submit it
+        if(tabId in pageViewStorage.windows[tab.windowId]) {
+            sendAndDeleteTab(tab.windowId, tab.id);
+        }
 
         // Store this new page
         pageViewStorage[tab.windowId][tab.id] = page;
-
-
     }
 });
 
-// Send the tab object, then remove from the pageViewStorage
-// Only use when closing a tab or window
-function sendAndDeleteTab(windowId, tabId) {
-    console.log('Sending: ' + page.url);
+// Listen for a tab being closed. Submit this tab and remove from storage
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    // If a window is being closed, ignore this event
+    if(removeInfo[isWindowClosing]) {
+        return;
+    }
 
+    // else
+    for(var windowId in pageViewStorage.windows) {
+        if(tabId in pageViewStorage.windows[windowId]) {
+            sendAndDeleteTab(windowId, tabId);
+            return;
+        }
+    }
+
+    // Maybe it doesn't exist? That's weird
+});
+
+function preparePageForSend(page) {
+    page.pageCloseTime = (new Date).getTime();
+
+    return page;
+}
+
+// Send the tab object, then remove from the pageViewStorage
+function sendAndDeleteTab(windowId, tabId) {
     // Retrieve the page
+    page = pageViewStorage.windows[windowId][tabId];
+
+    // Prepare the page
+    page = preparePageForSend(page);
 
     // Send the page
     post('http://ec2-174-129-49-253.compute-1.amazonaws.com/pageview', page);
