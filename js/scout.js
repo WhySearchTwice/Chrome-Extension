83cdaaa4bc6de12229c8089c938d9a8c24b76438
Ansel Santosa <ansel@anselsantosa.com>
2013-02-10 15:18:53 -0800

    removed log messages from scout

--- js/scout.js
+++ js/scout.js
@@ -7,13 +7,11 @@
  * Opens history replacement page manually in a new tab with [ Shift ] + [ H ]
  */
 document.addEventListener('keypress', function(event) {
-    console.log(event);
     var isInput =
         event.target.tagName === 'INPUT' ||
         event.target.tagName === 'TEXTAREA' ||
         event.target.hasAttribute('contenteditable')
     ;
-    console.log(isInput);
     if (event.keyCode === 72 && !isInput) {
         chrome.extension.sendMessage({ action: 'openHistory' });
     }
