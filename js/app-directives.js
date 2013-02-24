7f0312f0c88ffa5699998eaf7a1bf65f676cb421
Chris Gilbert <chris@cgilbert.net>
2013-02-23 16:32:33 -0800

    Made new directive for Kinetic and tested it by adding a method that just draws 10 random shapes.

--- js/app-directives.js
+++ js/app-directives.js
@@ -4,4 +4,43 @@ angular.module('history.directives', [])
             elm.text(version);
         };
     }])
+    .directive('kinetic', function() {
+        var kineticContainer = '<div ng-dblclick="drawShapes()" id="container"></div>';
+        return {
+            restrict: 'E',
+            compile:function (tElement, tAttrs, transclude) {
+                tElement.html(kineticContainer);
+
+                return function (scope, element, attrs) {
+                    scope.stage = new Kinetic.Stage({
+                        container: 'container',
+                        width: 578,
+                        height: 363
+                    });
+
+                    scope.drawShapes = function() {
+                        var layer = new Kinetic.Layer();
+
+                        var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+                        for(var n = 0; n < 10; n++) {
+                            var shape = new Kinetic.RegularPolygon({
+                                x: Math.random() * scope.stage.getWidth(),
+                                y: Math.random() * scope.stage.getHeight(),
+                                sides: Math.ceil(Math.random() * 5 + 3),
+                                radius: Math.random() * 100 + 20,
+                                fill: colors[Math.round(Math.random() * 5)],
+                                stroke: 'black',
+                                alpha: Math.random(),
+                                strokeWidth: 4,
+                                draggable: true
+                            });
+                            layer.add(shape);
+                        }
+
+                        scope.stage.add(layer);
+                    };
+                };
+            }
+        };
+    })
 ;
--- js/history.js
+++ js/history.js
@@ -2,7 +2,6 @@
 angular.module('history', ['history.filters', 'history.services', 'history.directives'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
         $routeProvider.otherwise({redirectTo: '/view1'});
     }])
 ;
