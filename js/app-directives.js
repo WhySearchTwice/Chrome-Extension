c5fd443132964ab1f4d684f83c3d33a051accebc
Chris Gilbert <chris@cgilbert.net>
2013-02-23 16:35:32 -0800

    Added message to make it more clear.

--- js/app-directives.js
+++ js/app-directives.js
@@ -6,10 +6,12 @@ angular.module('history.directives', [])
     }])
     .directive('kinetic', function() {
         var kineticContainer = '<div ng-dblclick="drawShapes()" id="container"></div>';
+        var helpMessage = '<h1>Double click the space above this.</h1>';
         return {
             restrict: 'E',
             compile:function (tElement, tAttrs, transclude) {
                 tElement.html(kineticContainer);
+                tElement.append(helpMessage);
 
                 return function (scope, element, attrs) {
                     scope.stage = new Kinetic.Stage({
