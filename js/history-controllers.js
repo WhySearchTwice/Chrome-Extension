20b5ad29bb6d614c908da47767f880ee0e10f83e
Chris Gilbert <chris@cgilbert.net>
2013-03-14 20:22:28 -0700

    Made directive correctly laid out and also made it draw based off a value in the controller.

--- js/history-controllers.js
+++ js/history-controllers.js
@@ -1,38 +1,3 @@
 function KineticCtrl($scope) {
-    $scope.stage = new Kinetic.Stage({
-        container: 'container',
-        width: window.innerWidth,
-        height: window.innerHeight - $('.navbar').outerHeight()
-    });
-
-    $scope.drawNode = function(posx, posy) {
-        var layer = new Kinetic.Layer();
-        var group = new Kinetic.Group({
-            x: posx,
-            y: posy
-        });
-
-        var line = new Kinetic.Line({
-            points: [0, 25, 500, 25],
-            stroke: 'black',
-            strokeWidth: 4
-        });
-
-        var text = new Kinetic.Text({
-            text: 'www.google.com',
-            fontSize: 18,
-            fontFamily: 'FontAwesome',
-            fill: '#555',
-            width: 300
-        });
-
-        group.add(line);
-        group.add(text);
-        layer.add(group);
-        $scope.stage.add(layer);
-    };
-
-    $scope.drawNode(200, 400);
-    $scope.drawNode(800, 300);
-    $scope.drawNode(400, 100);
+$scope.nodes = 4;
 }
--- js/history-directives.js
+++ js/history-directives.js
@@ -1,10 +1,50 @@
 angular.module('history.directives', [])
-    .directive('kinetic', function() {
-        var kineticContainer = '<div ng-dblclick="drawShapes()" id="container"></div>';
+    .directive('kinetic', function($timeout) {
         return {
             restrict: 'E',
-            compile:function (tElement, tAttrs, transclude) {
-                tElement.html(kineticContainer);
+            replace: true,
+            template: '<div id="container"></div>',
+            link:function postLink(scope, iElement, iAttrs) {
+                scope.stage = new Kinetic.Stage({
+                    container: 'container',
+                    width: window.innerWidth,
+                    height: window.innerHeight - $('.navbar').outerHeight()
+                });
+
+                scope.drawAll = function() {
+                    for (var i = 0; i < scope.nodes; i++) {
+                        scope.drawNode(i*200 + 300, i*150 + 200);
+                    }
+                };
+
+                scope.drawNode = function(posx, posy) {
+                    var layer = new Kinetic.Layer();
+                    var group = new Kinetic.Group({
+                        x: posx,
+                        y: posy
+                    });
+
+                    var line = new Kinetic.Line({
+                        points: [0, 25, 500, 25],
+                        stroke: 'black',
+                        strokeWidth: 4
+                    });
+
+                    var text = new Kinetic.Text({
+                        text: 'www.google.com',
+                        fontSize: 18,
+                        fontFamily: 'FontAwesome',
+                        fill: '#555',
+                        width: 300
+                    });
+
+                    group.add(line);
+                    group.add(text);
+                    layer.add(group);
+                    scope.stage.add(layer);
+                };
+
+                scope.drawAll();
             },
             controller: KineticCtrl
         };
