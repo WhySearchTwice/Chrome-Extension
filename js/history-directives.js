4f46633b29a48a7409ea34bae3bec989d5496db7
Ansel Santosa <ansel@anselsantosa.com>
2013-04-14 13:20:13 -0700

    Start refactoring drawing to work on single layer with nested groups

--- js/history-directives.js
+++ js/history-directives.js
@@ -18,13 +18,20 @@ angular.module('history.directives', [])
                 $scope.$watch('tree.built', function() {
                     $scope.pixelRatio = (window.innerWidth - $scope.offset) / (1000 * 60 * $scope.range);   // pixelRatio = effective canvas / range in ms
                     $scope.leftTime = (new Date()).getTime() - (1000 * 60 * $scope.range);                  // leftTime = now - range in ms
+                    var layer = new Kinetic.Layer();
+                    var y = $scope.lineHeight;
                     for (var subtree in $scope.tree.built.root) {
-                        $scope.drawSubtree($scope.tree.built.root[subtree]);
+                        var group = $scope.createSubtree($scope.tree.built.root[subtree]);
+                        group.setY(y);
+                        layer.add(group);
+                        y += $scope.lineHeight;
                     }
+                    console.log(layer);
                     $scope.stage.setSize({
                         width: $scope.viewportWidth,
                         height: $scope.lineHeight * 35
                     });
+                    $scope.stage.add(layer);
                 }, true);
 
                 $scope.viewportWidth = $window.innerWidth;
@@ -45,28 +52,31 @@ angular.module('history.directives', [])
                  * Recursively draw all nodes in a subtree tree
                  * @author chris, ansel
                  *
-                 * @param {Object} subtree subtree to be subtree
+                 * @param  {Object}       subtree subtree to be subtree
+                 * @return {KineticGroup}         Kinetic group of groups to draw
                  */
-                $scope.drawSubtree = function(subtree) {
+                $scope.createSubtree = function(subtree) {
                     console.log('Drawing:');
                     console.log(subtree);
+                    var group = new Kinetic.Group();
                     if (subtree.node) {
                         var start = $scope.pixelRatio * (subtree.node.pageOpenTime - $scope.leftTime);
                         var end = subtree.node.pageCloseTime ? start : window.innerWidth - $scope.offset;
-                        var group = new Kinetic.Group({
-                            x: start,
-                            y: $scope.lineHeight
-                        });
-                        $scope.drawNode(start, end, group, subtree.node);
+                        group.add($scope.createNode(start, end, subtree.node));
                     }
                     if (subtree.successor) {
-                        $scope.drawSubtree(subtree.successor);
+                        group.add($scope.createSubtree(subtree.successor));
                     }
                     if (subtree.children) {
+                        var y = $scope.lineHeight * 2;
                         for (var child in subtree.children) {
-                            $scope.drawSubtree(subtree.children[child]);
+                            var subgroup = $scope.createSubtree(subtree.children[child]);
+                            subgroup.setY(y);
+                            group.add(subgroup);
+                            y += $scope.lineHeight;
                         }
                     }
+                    return group;
                 };
 
                 /**
@@ -78,11 +88,15 @@ angular.module('history.directives', [])
                  * @param  group          group for node
                  * @param  node           Information about page view
                  */
-                $scope.drawNode = function(start, end, group, node) {
-                    var layer = new Kinetic.Layer();
+                $scope.createNode = function(start, end, node) {
+                    var group = new Kinetic.Group({
+                        x: start,
+                        y: $scope.lineHeight
+                    });
+                    var y = $scope.lineHeight;
 
                     var line = new Kinetic.Line({
-                        points: [0, 15, end - start, 15],
+                        points: [0, y + 15, end - start, y + 15],
                         stroke: end === window.innerWidth - $scope.offset ? 'blue' : 'black', // blue == still open
                         strokeWidth: 4
                     });
@@ -104,8 +118,7 @@ angular.module('history.directives', [])
 
                     group.add(line);
                     group.add(text);
-                    layer.add(group);
-                    $scope.stage.add(layer);
+                    return group;
                 };
 
                 // draw now line
